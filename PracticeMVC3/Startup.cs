326aaf4517db21934377e6f1637979051fee465f
RichFelton <rfelton.math@gmail.com>
2018-07-10 11:52:49 -0400

    first commit

---
+++ PracticeMVC3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PracticeMVC3.Startup))]
+namespace PracticeMVC3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
